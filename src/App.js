dd41db47f44cfcc683d4a253da312931c283977a
Bartosz Slowi <bartosz.slowi@gmail.com>
2025-07-06 14:08:03 +0200

    .

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import "./App.css";
 
-export default function BunnyHopeLandingPage() {
+export default function App() {
   return (
       <div className="landing-container">
         {/* Header */}
