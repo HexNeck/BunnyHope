7a11f06d9844f43b621a1f8a5f86a7bf6e0c481a
Bartosz Slowi <bartosz.slowi@gmail.com>
2025-07-08 22:39:41 +0200

    .

--- src/App.js
+++ src/App.js
@@ -65,63 +65,35 @@ export default function App() {
             <footer className="landing-footer">
                 <h2>Stay Updated!</h2>
                 <p>Sign up to get notified when Bunny Hope is ready!</p>
-                <div id="mc_embed_shell">
-                    <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet"
-                          type="text/css"/>
-                    <style type="text/css">
-                        #mc_embed_signup{background:#fff; false;clear:left; font:14px Helvetica,Arial,sans-serif; width: 600px;}
-                        /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-                        We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-                    </style>
-                    <div id="mc_embed_signup">
-                        <form
-                            action="https://hexlinggames.us9.list-manage.com/subscribe/post?u=c9620bb1e2220de89024fca2d&amp;id=abb5cc1784&amp;f_id=00e6d7e3f0"
-                            method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form"
-                            className="validate" target="_blank">
-                            <div id="mc_embed_signup_scroll"><h2>Subscribe</h2>
-                                <div className="indicates-required"><span className="asterisk">*</span> indicates
-                                    required
-                                </div>
-                                <div className="mc-field-group"><label htmlFor="mce-EMAIL">Email Address <span
-                                    className="asterisk">*</span></label><input type="email" name="EMAIL"
-                                                                                className="required email"
-                                                                                id="mce-EMAIL" required="" value=""/>
-                                </div>
-                                <div id="mce-responses" className="clear foot">
-                                    <div className="response" id="mce-error-response" style="display: none;"></div>
-                                    <div className="response" id="mce-success-response" style="display: none;"></div>
-                                </div>
-                                <div aria-hidden="true" style="position: absolute; left: -5000px;">
-                                    /* real people should not fill this in and expect good things - do not remove this
-                                    or risk form bot signups */
-                                    <input type="text" name="b_c9620bb1e2220de89024fca2d_abb5cc1784" tabIndex="-1"
-                                           value=""/>
-                                </div>
-                                <div className="optionalParent">
-                                    <div className="clear foot">
-                                        <input type="submit" name="subscribe" id="mc-embedded-subscribe"
-                                               className="button" value="Subscribe"/>
-                                        <p style="margin: 0px auto;"><a href="http://eepurl.com/jiO0Ts"
-                                                                        title="Mailchimp - email marketing made easy and fun"><span
-                                            style="display: inline-block; background-color: transparent; border-radius: 4px;"><img
-                                            className="refferal_badge"
-                                            src="https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_us/images/intuit-mc-rewards-text-dark.svg"
-                                            alt="Intuit Mailchimp"
-                                            style="width: 220px; height: 40px; display: flex; padding: 2px 0px; justify-content: center; align-items: center;"/></span></a>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <script type="text/javascript"
-                            src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
-                    <script
-                        type="text/javascript">(function ($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';}(jQuery)
-                    )
-                    ;var $mcj = jQuery.noConflict(true);</script>
-                </div>
 
+                <form
+                    action="https://hexlinggames.us9.list-manage.com/subscribe/post?u=c9620bb1e2220de89024fca2d&amp;id=abb5cc1784&amp;f_id=00e6d7e3f0"
+                    method="post"
+                    target="_blank"
+                    noValidate
+                    className="mailchimp-form"
+                >
+                    <label htmlFor="mce-EMAIL">Email Address *</label>
+                    <input
+                        type="email"
+                        name="EMAIL"
+                        id="mce-EMAIL"
+                        required
+                        placeholder="Your email address"
+                    />
+
+                    {/* Bot protection field */}
+                    <div style={{ position: "absolute", left: "-5000px" }} aria-hidden="true">
+                        <input
+                            type="text"
+                            name="b_c9620bb1e2220de89024fca2d_abb5cc1784"
+                            tabIndex="-1"
+                            defaultValue=""
+                        />
+                    </div>
+
+                    <button type="submit">Subscribe</button>
+                </form>
             </footer>
         </div>
     );
